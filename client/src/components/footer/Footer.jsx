1efaf1406e551d5a074c312b4455ee97d2086424
fermin234 <fermin234@hotmail.com>
2023-04-24 18:28:43 -0300

    changes styles

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
         <div className="w-full">
           <div className="md:flex md:justify-between md:w-full md:items-center">
             <div className="mb-6 md:mb-0 flex justify-center">
-              <img src={logo} alt="TyJ_Logo" className='w-60 h-60 md:w-40 md:h-40 hover:cursor-pointer hover:opacity-50' onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })} />
+              <img src={logo} alt="TyJ_Logo" className='w-60 h-60 md:w-40 md:h-40 hover:cursor-pointer hover:opacity-50 scale-150' onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })} />
             </div>
             <div className='flex flex-col items-center mb-6'>
               <h2 className="text-sm font-semibold text-gray-900 uppercase mb-2 font-Montserrat">Seguinos</h2>
--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -10,10 +10,10 @@ export default function NavBar() {
     <nav className="bg-[#368a8c]">
       <div className='absolute left-5 gap-5 mt-4 hidden md:flex'>
         <a href="https://www.instagram.com/tjinmobiliria/" target='_blank'>
-          <AiOutlineInstagram size={"1.5rem"} className='hover:opacity-50' />
+          <AiOutlineInstagram size={"1.5rem"} className='hover:opacity-50' color='white' />
         </a>
         <a href="https://wa.me/2473509269?text=Hola%20me%20interesaria%20charlar%20sobre%20una%20propiedad%20en%20especifico." target="_blank">
-          <AiOutlineWhatsApp size={"1.5rem"} className='hover:opacity-50' />
+          <AiOutlineWhatsApp size={"1.5rem"} className='hover:opacity-50' color='white' />
         </a>
       </div>
       <div className="flex flex-wrap items-center justify-end p-4 w-full" >
@@ -21,7 +21,7 @@ export default function NavBar() {
         {/* responsive */}
         <div className="flex w-full justify-between items-center md:order-2 md:hidden">
           <Link to="/">
-            <img src={logo} className="w-40" alt="inmobiliaria_Logo" />
+            <img src={logo} className="w-40 scale-150" alt="inmobiliaria_Logo" />
           </Link>
           <button data-collapse-toggle="navbar-search" type="button" className="h-fit inline-flex items-center p-2 bg-white text-sm text-gray-500 rounded-lg md:hidden  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-search" aria-expanded="false">
             <span className="sr-only">Open menu</span>
@@ -33,12 +33,12 @@ export default function NavBar() {
         <div className="items-center justify-center hidden w-full md:flex" id="navbar-search">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-[#368a8c] md:items-center">
             <li className='h-fit'>
-              <Link to="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent hover:text-white md:p-0">
+              <Link to="/" className="block py-2 pl-3 pr-4 text-black md:text-white  rounded hover:bg-gray-100 md:hover:bg-transparent hover:text-black md:p-0">
                 Inicio
               </Link>
             </li>
             {/* <li className='hidden md:flex h-fit'>
-              <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-white md:p-0 md:w-auto">
+              <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-black md:p-0 md:w-auto">
                 Comprar
                 <svg className="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
@@ -52,27 +52,27 @@ export default function NavBar() {
               </div>
             </li> */}
             <li className='h-fit'>
-              <a href='#campos' className=" hover:cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">
+              <a href='#campos' className=" hover:cursor-pointer block py-2 pl-3 pr-4 text-black md:text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">
                 Campos
               </a>
             </li>
             <li className="hidden md:flex h-fit">
               <Link to="/">
-                <img src={logo} className="w-40 h-40 hover:opacity-50" alt="inmobiliaria_Logo" />
+                <img src={logo} className="w-40 h-40 hover:opacity-50 scale-150" alt="inmobiliaria_Logo" />
               </Link>
             </li>
             <li className='h-fit'>
-              <Link to="/nosotros" className=" block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">
+              <Link to="/nosotros" className=" block py-2 pl-3 pr-4 text-black md:text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">
                 Nosotros
               </Link>
             </li>
             <li className='h-fit'>
-              <Link to="/contacto" className=" block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">
+              <Link to="/contacto" className=" block py-2 pl-3 pr-4 text-black md:text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">
                 Contacto
               </Link>
             </li>
             <li className='h-fit'>
-              <a href="tel:2473509269" target="_blank" className="md:hidden block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">
+              <a href="tel:2473509269" target="_blank" className="md:hidden block py-2 pl-3 pr-4 text-black md:text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">
                 Llamanos
               </a>
             </li>
