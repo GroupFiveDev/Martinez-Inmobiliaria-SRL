a5c607cc3df96f74cdc09b8bc2a91b06f48e86f4
fermin234 <fermin234@hotmail.com>
2023-04-30 15:22:46 -0300

    change class a className

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     <>
       {/* Nosotros */}
       <div className="relative h-[500px] div-about flex justify-center items-center">
-        <h1 class="text-6xl text-white flex font-Montserrat">Nosotros</h1>
+        <h1 className="text-6xl text-white flex font-Montserrat">Nosotros</h1>
       </div>
 
       {/* Info */}
@@ -13,12 +13,12 @@ export default function About() {
         </div>
         <div className="absolute bottom-0 right-0 hidden h-full lg:flex w-24 z-40 rallado">
         </div>
-        <p class="text-lg text-center text-white ">Brindamos servicios inmobiliarios exclusivamente de compra/venta y tasaciones urbanas y rurales mediante prácticas modernas para que nuestro cliente pueda alcanzar sus objetivos en el menor tiempo posible, sin estrés, generando una experiencia digna de repetir o recomendar.  Generalmente nuestro servicio comienza realizando una valuación del inmueble, creando informes de alta calidad con mucha información de valor para que cada cliente pueda tener una visión clara del mercado y comprender de forma detallada qué factores determinan el valor de su propiedad.</p>
+        <p className="text-lg text-center text-white ">Brindamos servicios inmobiliarios exclusivamente de compra/venta y tasaciones urbanas y rurales mediante prácticas modernas para que nuestro cliente pueda alcanzar sus objetivos en el menor tiempo posible, sin estrés, generando una experiencia digna de repetir o recomendar.  Generalmente nuestro servicio comienza realizando una valuación del inmueble, creando informes de alta calidad con mucha información de valor para que cada cliente pueda tener una visión clara del mercado y comprender de forma detallada qué factores determinan el valor de su propiedad.</p>
       </div>
 
       {/* Nuestros valores */}
       <div className="w-full flex flex-col items-center justify-center bg-[#368a8c] pb-5">
-        <h1 class="text-6xl text-gray-600 flex text-center my-10 font-Montserrat">Nuestros valores</h1>
+        <h1 className="text-6xl text-gray-600 flex text-center my-10 font-Montserrat">Nuestros valores</h1>
         <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 
           <div className="flex flex-col items-center text-center w-80">
@@ -27,8 +27,8 @@ export default function About() {
             </div>
 
             <div className="mt-3">
-              <p class="text-2xl text-gray-900 font-Montserrat">VOCACIÓN DE SERVICIO</p>
-              <p class="text-md mt-4 text-gray-900 ">Siempre estamos al servicio del cliente generando soluciones para cada necesidad.</p>
+              <p className="text-2xl text-gray-900 font-Montserrat">VOCACIÓN DE SERVICIO</p>
+              <p className="text-md mt-4 text-gray-900 ">Siempre estamos al servicio del cliente generando soluciones para cada necesidad.</p>
             </div>
           </div>
 
@@ -38,8 +38,8 @@ export default function About() {
             </div>
 
             <div className="mt-3">
-              <p class="text-2xl text-gray-900 font-Montserrat">SIMPLICIDAD</p>
-              <p class="text-md mt-4 text-gray-900 ">Creemos que si el proceso es simple, es doblemente bueno.</p>
+              <p className="text-2xl text-gray-900 font-Montserrat">SIMPLICIDAD</p>
+              <p className="text-md mt-4 text-gray-900 ">Creemos que si el proceso es simple, es doblemente bueno.</p>
             </div>
           </div>
 
@@ -49,8 +49,8 @@ export default function About() {
             </div>
 
             <div className="mt-3">
-              <p class="text-2xl text-gray-900 font-Montserrat">COMPROMISO</p>
-              <p class="text-md mt-4 text-gray-900 ">Tenemos la capacidad de sentir los objetivos del cliente como objetivos propios y eso nos facilita entender sus motivaciones y objetivos.</p>
+              <p className="text-2xl text-gray-900 font-Montserrat">COMPROMISO</p>
+              <p className="text-md mt-4 text-gray-900 ">Tenemos la capacidad de sentir los objetivos del cliente como objetivos propios y eso nos facilita entender sus motivaciones y objetivos.</p>
             </div>
           </div>
 
@@ -60,8 +60,8 @@ export default function About() {
             </div>
 
             <div className="mt-3">
-              <p class="text-2xl text-gray-900 font-Montserrat">ESTAR MOTIVADOS</p>
-              <p class="text-md mt-4 text-gray-900 ">Nos mantiene decididos y ágiles.</p>
+              <p className="text-2xl text-gray-900 font-Montserrat">ESTAR MOTIVADOS</p>
+              <p className="text-md mt-4 text-gray-900 ">Nos mantiene decididos y ágiles.</p>
             </div>
           </div>
 
@@ -71,8 +71,8 @@ export default function About() {
             </div>
 
             <div className="mt-3">
-              <p class="text-2xl text-gray-900 font-Montserrat">LOGROS COMPARTIDOS</p>
-              <p class="text-md mt-4 text-gray-900 ">Individualmente somos buenos, pero juntos somos geniales.</p>
+              <p className="text-2xl text-gray-900 font-Montserrat">LOGROS COMPARTIDOS</p>
+              <p className="text-md mt-4 text-gray-900 ">Individualmente somos buenos, pero juntos somos geniales.</p>
             </div>
           </div>
 
@@ -82,8 +82,8 @@ export default function About() {
             </div>
 
             <div className="mt-3">
-              <p class="text-2xl text-gray-900 font-Montserrat">CONOCIMIENTO</p>
-              <p class="text-md mt-4 text-gray-900 ">Informamos el paso a paso en cada tipo de proceso.</p>
+              <p className="text-2xl text-gray-900 font-Montserrat">CONOCIMIENTO</p>
+              <p className="text-md mt-4 text-gray-900 ">Informamos el paso a paso en cada tipo de proceso.</p>
             </div>
           </div>
 
@@ -97,8 +97,8 @@ export default function About() {
         </div>
 
         <div className="md:w-[50%] bg-[#51535b] h-full pl-5 py-5">
-          <h1 class="text-4xl text-white flex text-center my-10 font-Montserrat">¿CÓMO?</h1>
-          <p class="text-lg text-white ">Tratamos a cada cliente de forma personalizada para que tenga un trato exclusivo generando emociones positivas, manteniendo el foco en mejorar su calidad de vida.
+          <h1 className="text-4xl text-white flex text-center my-10 font-Montserrat">¿CÓMO?</h1>
+          <p className="text-lg text-white ">Tratamos a cada cliente de forma personalizada para que tenga un trato exclusivo generando emociones positivas, manteniendo el foco en mejorar su calidad de vida.
             <br />
             <br />
             En primer lugar entendiendo sus necesidades, y luego elaborando planes de trabajo conjunto de forma clara, para garantizar que todo lo que se promete se realizará en tiempo y forma,  trabajando de forma incansable, con dedicación y una verdadera pasión por el servicio al cliente para cumplir nuestro compromiso asumido.</p>
