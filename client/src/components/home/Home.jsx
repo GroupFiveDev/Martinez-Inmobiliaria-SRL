248de5006b05ef640cb0dfb066f48cc41b5cb445
fermin234 <fermin234@hotmail.com>
2023-04-30 15:21:57 -0300

    change drawer

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -9,8 +9,6 @@ export default function Home() {
   return (
     <>
       <div className='h-[300px] md:h-[600px] div-image-home' />
-      <button onClick={openModal} className="fixed top-0 left-0 bg-red-500 p-5 rounded-2xl z-50">ADMIN</button>
-      <Drawer isOpen={isOpen} closeModal={closeModal} />
       <Fields />
       <WhatsApp />
     </>
