9f3eb70ecee26a5494f303f3f8a179961f575bbc
fermin234 <fermin234@hotmail.com>
2023-05-01 14:19:08 -0300

    changes z-index

--- client/src/components/whatsApp/WhatsApp.jsx
+++ client/src/components/whatsApp/WhatsApp.jsx
@@ -8,7 +8,7 @@ export default function WhatsApp() {
   const location = useLocation()
   return (
     <>
-      <div className={`fixed bottom-4 left-4 bg-[#2d6c6d] rounded-full p-2 hover:cursor-pointer ${location.pathname === "/contacto" ? "hidden" : ""}`}>
+      <div className={`fixed bottom-4 left-4 bg-[#2d6c6d] z-50 rounded-full p-2 hover:cursor-pointer ${location.pathname === "/contacto" ? "hidden" : ""}`}>
         <a href="https://wa.me/2473509269?text=Hola%20me%20interesaria%20charlar%20sobre%20una%20propiedad%20en%20especifico." target="_blank">
           <AiOutlineWhatsApp className='w-12 h-12 ' color='white' />
         </a>
