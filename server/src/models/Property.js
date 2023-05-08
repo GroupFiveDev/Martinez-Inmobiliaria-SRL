bb8d1eae05c39cda9f9e72fede9243e9eef58fcd
fermin234 <fermin234@hotmail.com>
2023-05-08 15:02:21 -0300

    deleted models and create model properties

---
+++ server/src/models/Property.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = async (sequelize) => {
+  sequelize.define("property", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    type: {
+      type: DataTypes.ENUM("field", "apartment"),
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    hectares: {
+      type: DataTypes.INTEGER,
+    },
+    location: {
+      type: DataTypes.TEXT,
+    },
+    terrain: {
+      type: DataTypes.TEXT,
+    },
+    rooms: {
+      type: DataTypes.INTEGER,
+    },
+    bathrooms: {
+      type: DataTypes.INTEGER,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+    },
+    images: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
+      defaultValue: [],
+    },
+    archived: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    sold: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+};
