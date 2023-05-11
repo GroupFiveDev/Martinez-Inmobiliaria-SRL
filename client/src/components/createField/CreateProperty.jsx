e7974816e8c6d8c02394ccf9b8d7fbdd1a11a2b1
SantiRosso <santiagorosso@live.com.ar>
2023-05-11 14:21:12 -0300

    createProperty lógica

--- client/src/components/createField/CreateProperty.jsx
+++ client/src/components/createField/CreateProperty.jsx
@@ -1,63 +1,107 @@
+import axios from "axios";
+import { useState } from "react";
+
 const CreateProperty = () => {
 
-    //type title description hectares location terrain rooms bathrooms garage square price images
+    const [form, setForm] = useState({
+        type: "",
+        title: "",
+        description: "",
+        hectares: 0,
+        location: "",
+        terrain: "",
+        rooms: 0,
+        bathrooms: 0,
+        garage: 0,
+        square: 0,
+        price: 0, 
+        image: [],
+    })
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+        // console.log(form)
+    }
+
+    const handleSubmit = async (e) => {
+        try {
+            // console.log(form)
+            await axios.post("/properties", form)
+            alert("Propiedad Creada")
+        } catch (error) {
+            console.log(error)            
+        }
+    }
 
     return(
-        <div>
-            <h1 className="flex justify-center ">Crear Propiedad:</h1>
+        <div className="m-10">
+            <h1 className="flex justify-center font-bold mb-5">Crear Propiedad:</h1>
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="grid gap-6 mb-6 md:grid-cols-2">
                         <div>
-                            <label for="type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo de propiedad</label>
-                            <input type="text" id="type" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo de propiedad</label>
+                            <select onChange={handleChange} name="type" id="type" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="type">...</option>
+                                <option value="field">Campo</option>
+                                <option value="apartment">Departamento</option>
+                            </select>
                         </div>
                         <div>
-                            <label for="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Título</label>
-                            <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Título</label>
+                            <input onChange={handleChange} type="text" id="title" name="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                         </div>
                         <div>
-                            <label for="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripción</label>
-                            <input type="text" id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripción</label>
+                            <input onChange={handleChange} type="text" id="description" name="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                         </div>
                         <div>
-                            <label for="hectares" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hectareas</label>
-                            <input type="number" id="hectares" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hectareas</label>
+                            <input onChange={handleChange} type="number" id="hectares" name="hectares" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                         </div>
                         <div>
-                            <label for="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ubicación</label>
-                            <input type="text" id="location" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ubicación</label>
+                            <input onChange={handleChange} type="text" id="location" name="location" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                         </div>
                         <div>
-                            <label for="terrain" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Terreno</label>
-                            <input type="number" id="terrain" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Terreno</label> {/* Agrícola, Mixto o Ganadero */}
+                            <input onChange={handleChange} type="text" id="terrain" name="terrain" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div className="mb-6">
-                            <label for="rooms" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Habitaciones</label>
-                            <input type="number" id="rooms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Habitaciones</label>
+                            <input onChange={handleChange} type="number" id="rooms" name="rooms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div className="mb-6">
-                            <label for="bathrooms" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Baños</label>
-                            <input type="number" id="bathrooms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Baños</label>
+                            <input onChange={handleChange} type="number" id="bathrooms" name="bathrooms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div className="mb-6">
-                            <label for="garage" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Garage</label>
-                            <input type="number" id="garage" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Garage</label>
+                            <input onChange={handleChange} type="number" id="garage" name="garage" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div className="mb-6">
-                            <label for="garage" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Metros cuadrados</label>
-                            <input type="number" id="garage" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Metros cuadrados</label>
+                            <input onChange={handleChange} type="number" id="garage" name="garage" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div className="mb-6">
-                            <label for="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
-                            <input type="number" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
+                            <input onChange={handleChange} type="number" id="price" name="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                         </div>
                         <div className="mb-6">
-                            <label for="garage" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Imagenes</label>
-                            <input type="text" id="garage" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Imagenes</label>
+                            <input onChange={handleChange} type="text" id="images" name="images" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                         </div>
+                        {/* <div className="mb-6">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Imagenes</label>
+                            <input type="file" id="images" name="images" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                        </div> */}
                     </div>
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    <div className="flex justify-center items-center">
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear propiedad</button>
+                    </div>
                 </form>
             </div>
         </div>
