1eefb479984a0539ce348ab758e3ad905e4ea53a
fermin234 <fermin234@hotmail.com>
2023-05-13 23:07:19 -0300

    img feat

--- client/src/components/card/CardDetail.jsx
+++ client/src/components/card/CardDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams, Prompt, useHistory } from "react-router-dom";
 import { useModal } from "../../hooks/useModal.js";
 import { useAuth } from '../../context/authContext.jsx'
@@ -24,6 +24,8 @@ export default function CardDetail() {
   const [property, setProperty] = useState();
   const [change, setChange] = useState()
   const [imagesCount, setImagesCount] = useState(0)
+  const [principal, setPrincipal] = useState(1)
+  const title = useRef(null);
   const [edit, setEdit] = useState({
     type: null,
     title: null,
@@ -41,6 +43,8 @@ export default function CardDetail() {
   const { user } = useAuth()
 
   const handleImageClick = (index) => {
+    title.current.scrollIntoView({ behavior: 'smooth' });
+    setPrincipal(1)
     setActiveImageIndex(index);
   };
 
@@ -77,7 +81,10 @@ export default function CardDetail() {
     history.push("/")
   }
 
-  const handleLoadImages = () => {
+  const handleLoadImages = (e) => {
+    if (e.target.alt === "imagePrincipal") {
+      setPrincipal(0)
+    }
     if (imagesCount === property?.images.length + 1) {
       return
     }
@@ -122,7 +129,7 @@ export default function CardDetail() {
         <div className="absolute bottom-0 left-0 hidden h-full xl:flex w-24 z-40 rallado" />
         <div className="flex flex-col justify-center pt-4 w-[95%] xl:w-[60%]">
           {/* titulo */}
-          <div className="bg-[#368a8c] mb-5 flex items-center gap-2 w-full">
+          <div className="bg-[#368a8c] mb-5 flex items-center gap-2 w-full" ref={title}>
             <h1 className={`${edit.title ? "hidden" : ""} text-3xl text-start ml-3 font-bold text-white font-Montserrat`}>
               {property?.title}
             </h1>
@@ -138,13 +145,22 @@ export default function CardDetail() {
                 <h1 className="font-Montserrat text-white font-bold text-xl">Guardando cambios...</h1>
               </Loading>
               {/* imagen */}
-              <div className="px-2  xl:w-full">
-                <img
-                  src={id == 1 ? property?.images[activeImageIndex] : "https://img.freepik.com/foto-gratis/gran-paisaje-verde-cubierto-cesped-rodeado-arboles_181624-14827.jpg"}
-                  onLoad={handleLoadImages}
-                  alt={property?.title}
-                  className="w-full object-cover rounded-lg shadow-lg"
-                />
+              <div className="px-2 xl:w-full">
+                <div className="w-full relative flex justify-center items-center">
+                  <img
+                    src={id == 1 ? property?.images[activeImageIndex] : "https://img.freepik.com/foto-gratis/gran-paisaje-verde-cubierto-cesped-rodeado-arboles_181624-14827.jpg"}
+                    onLoad={handleLoadImages}
+                    alt="imagePrincipal"
+                    className={`${principal ? "opacity-25" : ""} w-full object-fill rounded-lg shadow-lg relative`}
+                  />
+                  <div role="status" className={`${principal ? "" : "hidden"} flex justify-center items-center w-[60px] h-[60px] absolute`}>
+                    <svg aria-hidden="true" className="w-full h-auto text-gray-200 animate-spin fill-[#368a8c]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                </div>
                 <div className="mt-4 grid grid-cols-3 gap-4">
                   {property?.images.map((image, index) => (
                     <button key={index} onClick={() => handleImageClick(index)}>
@@ -152,7 +168,7 @@ export default function CardDetail() {
                         src={id == 1 ? image : "https://img.freepik.com/foto-gratis/gran-paisaje-verde-cubierto-cesped-rodeado-arboles_181624-14827.jpg"}
                         onLoad={handleLoadImages}
                         alt={property?.title}
-                        className={`w-full object-cover rounded-lg shadow-lg ${index === activeImageIndex ? "border-2 border-blue-500" : ""}`}
+                        className={`h-[100%] rounded-lg shadow-lg ${index === activeImageIndex ? "border-2 border-blue-500" : ""}`}
                       />
                     </button>
                   ))}
