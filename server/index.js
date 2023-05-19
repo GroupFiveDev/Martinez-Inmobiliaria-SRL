60ef7646754de184952f753067bb57e35b9e7ff6
fermin234 <fermin234@hotmail.com>
2023-05-19 14:29:16 -0300

    log

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+require("dotenv").config();
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+console.log("**", DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, "**");
 
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
